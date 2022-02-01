e4c21a2509e1e2ab373cd94035b9152745958e70
Accacio <raccacio@poli.ufrj.br>
2022-02-01 01:20:16 +0100

    move defs to utils.h

--- ladder.c
+++ ladder.c
@@ -1,20 +1,9 @@
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 
-typedef uint32_t b32;
-
-typedef uint64_t u64;
-typedef uint32_t u32;
-typedef uint16_t u16;
-typedef uint8_t u8;
-
-typedef int64_t s64;
-typedef int32_t s32;
-typedef int16_t s16;
-typedef int8_t s8;
+#include "./utils.h"
 
 typedef struct
 {
